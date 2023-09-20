b7a6b0bcbea686be8cd417c84ab79fad11f51dcd
Jon Azpiazu <jonazpiazu@users.noreply.github.com>
2023-09-20 14:27:51 +0200

    Add the option to specify a branch as part of the repo url

--- jon_rocker/ros_git_pkg.py
+++ jon_rocker/ros_git_pkg.py
@@ -24,11 +24,33 @@ class RosGitPkg(RockerExtension):
         snippet = pkgutil.get_data(
             'jon_rocker',
             'templates/ros_git_pkg_snippet.Dockerfile.em').decode('utf-8')
-        print(f'url is {cli_args["ros_git_pkg"][0]}')
-        return em.expand(snippet, git_url=cli_args["ros_git_pkg"][0])
+        branch_name = None
+        branch_name = self.extractsuffix(self.removeprefix(cli_args["ros_git_pkg"][0], 'git@'))
+        git_url = cli_args["ros_git_pkg"][0]
+        if branch_name is not None:
+            git_url = self.removesuffix(cli_args["ros_git_pkg"][0], '@' + branch_name)
+        return em.expand(snippet, git_url = git_url, branch_name = branch_name)
 
     def get_docker_args(self, cli_args):
         return ''
+
+    # only available for python 3.9+
+    def removeprefix(self, text, prefix):
+        if text.startswith(prefix):
+            return text[len(prefix):]
+        return text
+
+    def extractsuffix(self, text):
+        if "@" in text:
+            suffix_start = text.index("@") + 1
+            return text[suffix_start:]
+        else:
+            return None
+
+    def removesuffix(self,text, suffix):
+        if text.endswith(suffix):
+            return text[:len(text) - len(suffix)]
+        return text
 
     @staticmethod
     def register_arguments(parser):
